d27d94bbc78c7e46aabdd93c6c2d36eefa4b31d3
Konstantin Grupp <coding@konstantin-grupp.de>
2024-04-26 09:48:07 +0200

    add total number

--- visualize/src/components/Visualizer.jsx
+++ visualize/src/components/Visualizer.jsx
@@ -14,26 +14,40 @@ import EmojiEventsIcon from '@mui/icons-material/EmojiEvents';
 import Link from "@mui/material/Link";
 
 export const Visualizer = () => {
-    return <Grid sx={{display: 'flex', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'center', gap: 1}}>{statistics.map((statistic, index) => (
-        <Card variant={"elevation"} key={statistic.user.id} sx={{width: 500}}>
-            <CardContent>
-                <Typography variant="h5" component="div" sx={{display: 'flex', justifyContent: 'space-between'}}>
-                    <Box>{index === 0 && <EmojiEventsIcon color={'primary'}/>}{statistic.user.name}</Box>
-                    <Box sx={{display: 'flex', gap: 1}}>
-                        <Chip icon={<ArticleIcon/>} label={statistic.totalBlogPosts}/>
-                        <Chip icon={<RecommendIcon/>} label={statistic.totalLikes}/>
-                    </Box>
-                </Typography>
-                <Typography variant="body2" sx={{display: 'flex', flexDirection: 'column', gap: 1, paddingTop: 1}}>
-                    {statistic.popularBlogPosts.map(blogPost => (
-                        <Box key={blogPost.title} sx={{display: 'flex', justifyContent: 'space-between'}}>
-                            <Link href={blogPost.link} underline={"none"} target={"_blank"}>{blogPost.title}</Link>
-                            {blogPost.likeCount > 0 &&
-                                <Chip size={'small'} sx={{marginLeft: 1}} icon={<RecommendIcon/>}
-                                      label={blogPost.likeCount}/>}
-                        </Box>))}
-                </Typography>
-            </CardContent>
-        </Card>
-    ))}</Grid>;
+    const totalBlogPosts = statistics.reduce((acc, statistic) => acc + statistic.totalBlogPosts, 0);
+    const totalLikes = statistics.reduce((acc, statistic) => acc + statistic.totalLikes, 0);
+    return <Box sx={{display: 'flex', flexDirection: 'column', justifyContent: 'center', gap: 1}}>
+        <Box sx={{display: 'flex', justifyContent: 'center', gap: 1, paddingBottom: 1, alignItems: 'center'}}>
+            <Chip icon={<ArticleIcon/>} label={totalBlogPosts}/>
+            <Chip icon={<RecommendIcon/>} label={totalLikes}/>
+        </Box>
+        <Grid sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+            gap: 1
+        }}>{statistics.map((statistic, index) => (
+            <Card variant={"elevation"} key={statistic.user.id} sx={{width: 500}}>
+                <CardContent>
+                    <Typography variant="h5" component="div" sx={{display: 'flex', justifyContent: 'space-between'}}>
+                        <Box>{index === 0 && <EmojiEventsIcon color={'primary'}/>}{statistic.user.name}</Box>
+                        <Box sx={{display: 'flex', gap: 1}}>
+                            <Chip icon={<ArticleIcon/>} label={statistic.totalBlogPosts}/>
+                            <Chip icon={<RecommendIcon/>} label={statistic.totalLikes}/>
+                        </Box>
+                    </Typography>
+                    <Typography variant="body2" sx={{display: 'flex', flexDirection: 'column', gap: 1, paddingTop: 1}}>
+                        {statistic.popularBlogPosts.map(blogPost => (
+                            <Box key={blogPost.title} sx={{display: 'flex', justifyContent: 'space-between'}}>
+                                <Link href={blogPost.link} underline={"none"} target={"_blank"}>{blogPost.title}</Link>
+                                {blogPost.likeCount > 0 &&
+                                    <Chip size={'small'} sx={{marginLeft: 1}} icon={<RecommendIcon/>}
+                                          label={blogPost.likeCount}/>}
+                            </Box>))}
+                    </Typography>
+                </CardContent>
+            </Card>
+        ))}</Grid>
+    </Box>;
 };
