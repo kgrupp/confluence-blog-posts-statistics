a8064048aec5854accc69ae94b2e0b8a26cb819b
Konstantin Grupp <coding@konstantin-grupp.de>
2024-07-20 22:30:45 +0200

    fix formatting

--- src/main/kotlin/de/kgrupp/confluence/statistics/convertToMarkdown.kt
+++ src/main/kotlin/de/kgrupp/confluence/statistics/convertToMarkdown.kt
@@ -10,9 +10,10 @@ fun List<UserStatistics>.convertToMarkdown(minCreatedDate: LocalDate): String {
   val builder = StringBuilder()
   val totalBlogPosts = this.sumOf { it.totalBlogPosts }
   val totalBlogPostLikes = this.sumOf { it.totalLikes }
-    builder.appendLine("# Statistics")
-    builder.append("\uD83D\uDCDC $totalBlogPosts")
-    builder.appendLine(" | \uD83D\uDC4D $totalBlogPostLikes")
+  builder.appendLine("# Statistics")
+  builder.append("\uD83D\uDCDC $totalBlogPosts")
+  builder.append(" | \uD83D\uDC4D $totalBlogPostLikes")
+  builder.appendLine()
   builder.appendLine("# Top recent blog posts")
   val oneMonthAgo = LocalDate.now().minusMonths(1)
   builder.appendLine("Created after ${oneMonthAgo.formatToDate()}")
@@ -20,28 +21,30 @@ fun List<UserStatistics>.convertToMarkdown(minCreatedDate: LocalDate): String {
       this.asSequence()
           .flatMap { it.popularBlogPosts }
           .filter { it.createdAt.atZone(ZoneId.of("UTC")).toLocalDate().isAfter(oneMonthAgo) }
-          .sortedBy { it.createdAt}
+          .sortedBy { it.createdAt }
           .sortedBy { -it.likeCount }
           .take(15)
           .toList()
   topRecentBlogPosts.forEachIndexed { index, blogPost ->
     builder.append("${index + 1}. [${blogPost.title}](${blogPost.link})")
-    builder.appendLine(" | \uD83D\uDCDC ${blogPost.author.name}")
-    builder.appendLine(" | \uD83D\uDC4D ${blogPost.likeCount}")
+    builder.append(" | \uD83D\uDCDC ${blogPost.author.name}")
+    builder.append(" | \uD83D\uDC4D ${blogPost.likeCount}")
+    builder.appendLine()
   }
 
   builder.appendLine("# Global Ranking")
-    builder.appendLine("Starting at ${minCreatedDate.formatToDate()}")
+  builder.appendLine("Starting at ${minCreatedDate.formatToDate()}")
 
   this.forEachIndexed { index, userStatistics ->
     val ranking = getRanking(index)
     builder.append("## ${ranking} ${userStatistics.user.name}")
     builder.append(" | \uD83D\uDCDC ${userStatistics.totalBlogPosts}")
     builder.append(" | \uD83D\uDC4D ${userStatistics.totalLikes}")
-      builder.appendLine()
+    builder.appendLine()
     userStatistics.popularBlogPosts.take(5).forEachIndexed { i, blogPost ->
-      builder.appendLine("${i + 1}. [${blogPost.title}](${blogPost.link})")
-      builder.appendLine(" | \uD83D\uDC4D ${blogPost.likeCount}\n")
+      builder.append("${i + 1}. [${blogPost.title}](${blogPost.link})")
+      builder.append(" | \uD83D\uDC4D ${blogPost.likeCount}\n")
+      builder.appendLine()
     }
     builder.appendLine()
   }
